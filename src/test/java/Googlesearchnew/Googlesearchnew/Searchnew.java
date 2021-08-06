63c9452a447d02f5df36ac1dfcecba66ab45b63d
sreenathmohanan <sreenath.mohan@ibsplc.com>
2021-08-06 09:13:16 -0500

    updated driver

--- src/test/java/Googlesearchnew/Googlesearchnew/Searchnew.java
+++ src/test/java/Googlesearchnew/Googlesearchnew/Searchnew.java
@@ -16,7 +16,7 @@ public  void test()
 {
 
 //Setting system properties of ChromeDriver
-System.setProperty("webdriver.chrome.driver","C://Users//232985//eclipse-workspace//Googlesearchnew//Googlesearchnew//Chromedriver//chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","https://github.com/sreenathmohanan/Googlesearchnew/tree/master/Chromedriver/chromedriver.exe");
 
 //Creating an object of ChromeDriver
 WebDriver driver = new ChromeDriver();
