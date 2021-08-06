f3eb344e7ff6af457ac8cbd3212f2ae7f5074d19
sreenathmohanan <sreenath.mohan@ibsplc.com>
2021-08-06 12:50:01 -0500

    Updated webpage

--- src/test/java/Googlesearchnew/Googlesearchnew/Searchnew.java
+++ src/test/java/Googlesearchnew/Googlesearchnew/Searchnew.java
@@ -6,41 +6,56 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Searchnew 
 {
 
+@BeforeTest
+
+public void start()
+
+{
+	
+	System.out.println("Program starting");	
+	
+}
+	
+	
 @Test
 public  void test()
 {
 	
-	
 
-//Setting system properties of ChromeDriver
 System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 
-//Creating an object of ChromeDriver
 WebDriver driver = new ChromeDriver();
 driver.manage().window().maximize();
 
-//Deleting all the cookies
 driver.manage().deleteAllCookies();
 
-//Specifying pageLoadTimeout and Implicit wait
 driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-//launching the specified URL
 driver.get("https://www.google.com/");
 
-//Locating the elements using name locator for the text box
 driver.findElement(By.name("q")).sendKeys("YouTube");
 
-//name locator for google search button
 WebElement searchIcon = driver.findElement(By.name("btnK"));
 searchIcon.click();
-driver.quit();
 
+driver.quit();
+}
+
+
+@AfterTest
+
+public void Endtest()
+{
+	
+	System.out.println("Program starting");	
 }
 }
+
